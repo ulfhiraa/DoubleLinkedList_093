e677e3289a8d40191529b3cb3f15cb3cdcc05a51
ulfhiraa <studyiraa@gmail.com>
2023-05-26 21:59:42 +0700

    method untuk menambah node

--- double_linkedliist/double_linkedliist.cpp
+++ double_linkedliist/double_linkedliist.cpp
@@ -28,3 +28,15 @@ public:
 DoubleLinkedList::DoubleLinkedList() {
 	START = NULL;
 }
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student : ";
+	cin >> nim;
+	cout << "\nEnter the name of the student : ";
+	cin >> nm;
+	Node* newNode = new Node(); //step 1
+	newNode->noMhs = nim; // step 2
+	newNode->name = nm; // step 2
+}
