ee3c2e7a39ffc4cd671d083ac356d3af1fa297f6
ulfhiraa <studyiraa@gmail.com>
2023-05-26 22:24:46 +0700

    method untuk mencari dengan bantuan previous dan current. juga terdapat proses perulangan while

--- double_linkedliist/double_linkedliist.cpp
+++ double_linkedliist/double_linkedliist.cpp
@@ -76,5 +76,12 @@ void DoubleLinkedList::addNode() {
 }
 
 bool DoubleLinkedList::search(int rollNo, Node** previous, Node** current) {
+	*previous = START; // step 1.a
+	*current = START; // 1.b
+	while (*current != NULL && rollNo != (*current)->noMhs) { // step 1.c
+		*previous = *current; // step 1.d
+		*current = (*current)->next; // step 1.e
 
+		return (*current != NULL);
+	}
 }
